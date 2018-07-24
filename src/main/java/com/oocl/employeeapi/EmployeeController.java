da1399ed78bec3454353ebeaa05c81378f93b4d6
Jerry <jerry.zhang@oocl.com>
2018-07-24 13:20:37 +0800

    [Jerry] change one employee to a list

--- src/main/java/com/oocl/employeeapi/EmployeeController.java
+++ src/main/java/com/oocl/employeeapi/EmployeeController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class EmployeeController {
 
@@ -12,8 +15,12 @@ public class EmployeeController {
 
     @GetMapping("employees")
     public Employee getEmployeeInfo(){
-        Employee employee = new Employee(1,"小明",20,"男");
-        return employeeService.showEmp(employee);
+        Employee employee1 = new Employee(1,"小明",20,"男");
+        Employee employee2 = new Employee(2,"小红",18,"女");
+        List<Employee> employeeList = new ArrayList<>();
+        employeeList.add(employee1);
+        employeeList.add(employee2);
+        return employeeService.showEmp(employeeList);
     }
 
 }
