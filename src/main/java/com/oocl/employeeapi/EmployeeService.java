c4edfc4dd0ddb7c622fcd2721e7013649573c2a0
Jerry <jerry.zhang@oocl.com>
2018-07-24 12:56:48 +0800

    [Jerry]add class Employee

---
+++ src/main/java/com/oocl/employeeapi/EmployeeService.java
@@ -0,0 +1,11 @@
+//package com.oocl.employeeapi;
+//
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EmployeeService {
+//
+//    public String showEmp(Employee employee){
+//
+//    }
+//}
