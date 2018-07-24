62c4bcb1d6cedc4edfecf233f39264c20e77ef41
Jerry <jerry.zhang@oocl.com>
2018-07-24 12:57:55 +0800

    [Jerry]add class EmployeeService

--- src/main/java/com/oocl/employeeapi/EmployeeController.java
+++ src/main/java/com/oocl/employeeapi/EmployeeController.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class EmployeeApiController {
+public class EmployeeController {
 
     @Autowired
     private Employee employee;
