8e8b1e6e93d5f7f2c450c9e0414748b0a466dc2f
Jerry <jerry.zhang@oocl.com>
2018-07-24 12:57:14 +0800

    [Jerry]add class EmployeeController

--- src/main/java/com/oocl/employeeapi/EmployeeApiController.java
+++ src/main/java/com/oocl/employeeapi/EmployeeApiController.java
@@ -1,11 +1,11 @@
-//package com.oocl.employeeapi;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class EmployeeApiController {
-//
-//    @Autowired
-//    private Employee employee;
-//}
+package com.oocl.employeeapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeApiController {
+
+    @Autowired
+    private Employee employee;
+}
